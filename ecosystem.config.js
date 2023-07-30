63a3c145638608c3d87f4b48c81f23aa370ce2a0
lucdq <lucdq410@gmail.com>
2023-07-30 23:54:49 +0700

    update ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "toothfairy be",
-            script: "index.js",
+            script: "server.js",
             max_memory_restart: "200M",
             restart_delay: 20000,
             exp_backoff_restart_delay: 100,
